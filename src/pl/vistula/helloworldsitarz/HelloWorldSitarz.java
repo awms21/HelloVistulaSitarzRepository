fa1aa0be7a91dd7a10287ab7da158e2707bfcfd1
awms21 <awms21@gmail.com>
2022-10-14 00:16:01 +0200

    Commit nb 3

--- src/pl/vistula/helloworldsitarz/HelloWorldSitarz.java
+++ src/pl/vistula/helloworldsitarz/HelloWorldSitarz.java
@@ -5,5 +5,7 @@ public class HelloWorldSitarz {
         System.out.println("Hello Vistula Sitarz");
 
         System.out.println("Hello Vistula 2");
+
+        System.out.println("Vistula to moja uczelnia");
     }
 }
