ed04c724fce194c486a1a2362d13e588404ca6da
awms21 <awms21@gmail.com>
2022-10-14 00:13:32 +0200

    Commit nb 2

--- src/pl/vistula/helloworldsitarz/HelloWorldSitarz.java
+++ src/pl/vistula/helloworldsitarz/HelloWorldSitarz.java
@@ -3,5 +3,7 @@ package pl.vistula.helloworldsitarz;
 public class HelloWorldSitarz {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Sitarz");
+
+        System.out.println("Hello Vistula 2");
     }
 }
